aa064056e41b82eddc0da0306261108ad7aa0120
Denis <Denis@DESKTOP-2179KCJ>
2023-10-24 11:58:33 +0200

    Add project files.

---
+++ eTicket/Controllers/ProducersController.cs
@@ -0,0 +1,22 @@
+﻿using eTicket.Data;
+using eTicket.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTicket.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly IProducersService _service;
+
+        public ProducersController(IProducersService service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _service.GetALlAsync();
+            return View(allProducers);
+        }
+    }
+}
---
+++ eTicket/Data/Services/ActorsServices.cs
@@ -0,0 +1,14 @@
+﻿using eTicket.Data.Base;
+using eTicket.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTicket.Data.Services
+{
+    public class ActorsServices : EntityBaseRepository<Actor>, IActorsServices
+    {
+       
+        public ActorsServices(AppDbContext context) : base(context) { }
+       
+     
+    }
+}
